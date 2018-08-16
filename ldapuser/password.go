ae66afc44950de980ceda74e3654424c968cbd92
Jarlyyn <jarlyyn@qq.com>
2018-08-17 01:58:39 +0800

    update

--- ldapuser/password.go
+++ ldapuser/password.go
@@ -1,8 +1,6 @@
 package ldapuser
 
 import (
-	"fmt"
-
 	"github.com/herb-go/member"
 	ldap "gopkg.in/ldap.v2"
 )
@@ -12,17 +10,13 @@ type PasswordProvider struct {
 }
 
 func (p *PasswordProvider) VerifyPassword(uid string, password string) (bool, error) {
-	l, err := p.Config.Dial()
-	defer l.Close()
+	l, err := p.Config.BindUser(uid, password)
 	if err != nil {
 		return false, err
 	}
-	err = l.Bind(fmt.Sprintf(p.Config.UserDN, uid), password)
+	defer l.Close()
 	if ldap.IsErrorWithCode(err, ldap.LDAPResultInvalidCredentials) {
 		return false, nil
-	}
-	if err != nil {
-		return false, err
 	}
 	return true, nil
 }
@@ -30,20 +24,7 @@ func (p *PasswordProvider) VerifyPassword(uid string, password string) (bool, er
 //UpdatePassword update user password
 //Return any error if raised
 func (p *PasswordProvider) UpdatePassword(uid string, password string) error {
-	l, err := p.Config.Dial()
-	if err != nil {
-		return err
-	}
-	defer l.Close()
-
-	err = l.Bind(p.Config.BindDN, p.Config.BindPass)
-	if err != nil {
-		return err
-	}
-	passwordModifyRequest := ldap.NewPasswordModifyRequest(fmt.Sprintf(p.Config.UserDN, uid), "", password)
-	_, err = l.PasswordModify(passwordModifyRequest)
-	return err
-
+	return p.Config.UpdatePassword(uid, password)
 }
 
 func (p *PasswordProvider) InstallToMember(service *member.Service) {
