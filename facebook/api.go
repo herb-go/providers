33732b966bdef72fc07fdf861b902e9dcbfa3a89
Jarlyyn <jarlyyn@qq.com>
2019-01-29 22:17:00 +0800

    update

---
+++ facebook/api.go
@@ -0,0 +1,32 @@
+package facebook
+
+import (
+	"net/http"
+
+	"github.com/herb-go/fetch"
+)
+
+var Server = fetch.Server{
+	Host:    "https://graph.facebook.com",
+	Headers: http.Header{},
+}
+
+var apiAccessToken = Server.EndPoint("POST", "/v3.2/oauth/access_token")
+var apiUser = Server.EndPoint("GET", "/v3.2/oauth/access_token")
+
+type ResultAPIAccessToken struct {
+	AccessToken string `json:"access_token"`
+	ExpiresIn   int    `json:"expires_in"`
+	TokenType   string `json:"token_type"`
+}
+
+type ResultAPIUser struct {
+	ID         string `json:"id"`
+	Email      string `json:"email"`
+	FirstName  string `json:"first_name"`
+	MiddleName string `json:"middle_name"`
+	LastName   string `json:"last_name"`
+	Gender     string `json:"gender"`
+	Name       string `json:"name"`
+	ProfilePic string `json:"profile_pic"`
+}
---
+++ facebook/app.go
@@ -0,0 +1,80 @@
+package facebook
+
+import (
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/herb-go/fetch"
+)
+
+type App struct {
+	ID      string
+	Key     string
+	Clients fetch.Clients
+}
+
+var DefaultFields = []string{
+	"id",
+	"email",
+	"first_name",
+	"middle_name",
+	"last_name",
+	"gender",
+	"name",
+	"profile_pic",
+}
+
+func (a *App) GetAccessToken(code string, redirect_url string) (*ResultAPIAccessToken, error) {
+	params := url.Values{}
+	params.Set("client_id", a.ID)
+	params.Set("client_secret", a.Key)
+	params.Set("code", code)
+	params.Set("redirect_uri", redirect_url)
+	req, err := apiAccessToken.NewRequest(nil, []byte(params.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	// req.Header.Set("Accept", "application/json")
+	rep, err := a.Clients.Fetch(req)
+	if err != nil {
+		return nil, err
+	}
+	if rep.StatusCode != http.StatusOK {
+		return nil, rep
+	}
+	result := &ResultAPIAccessToken{}
+	err = rep.UnmarshalAsJSON(result)
+	if err != nil {
+		return nil, err
+	}
+	if result.AccessToken == "" {
+		return nil, rep
+	}
+	return result, nil
+}
+func (a *App) GetUser(accessToken string) (*ResultAPIUser, error) {
+	params := url.Values{}
+	params.Set("access_token", accessToken)
+	params.Set("field", strings.Join(DefaultFields, ","))
+	req, err := apiUser.NewRequest(params, nil)
+	if err != nil {
+		return nil, err
+	}
+	rep, err := a.Clients.Fetch(req)
+	if err != nil {
+		return nil, err
+	}
+	if rep.StatusCode != http.StatusOK {
+		return nil, rep
+	}
+	result := &ResultAPIUser{}
+	err = rep.UnmarshalAsJSON(result)
+	if err != nil {
+		return nil, err
+	}
+	if result.ID == "" {
+		return nil, rep
+	}
+	return result, nil
+}
