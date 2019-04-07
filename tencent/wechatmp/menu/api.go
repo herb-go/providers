ad39e2269168e5554bc6c3b9bc8a105e16ed601d
Jarlyyn <jarlyyn@qq.com>
2019-04-08 01:28:12 +0800

    update

--- tencent/wechatmp/menu/api.go
+++ tencent/wechatmp/menu/api.go
@@ -1,6 +1,7 @@
 package menu
 
 import (
+	"github.com/herb-go/fetch"
 	"github.com/herb-go/providers/tencent/wechatmp"
 )
 
@@ -12,5 +13,8 @@ func CreateMenu(App *wechatmp.App, menu *Menu) error {
 func GetMenu(App *wechatmp.App) (*MenuResult, error) {
 	menu := NewMenuResult()
 	err := App.CallJSONApiWithAccessToken(wechatmp.APIMenuGet, nil, nil, menu)
+	if fetch.CompareAPIErrCode(err, "46003") {
+		return menu, nil
+	}
 	return menu, err
 }
