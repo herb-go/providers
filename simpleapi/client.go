7dcb3b5011f7fe689d8795385bdb0bc5ae02a88d
Jarlyyn <jarlyyn@qq.com>
2019-11-20 00:19:44 +0800

    update

--- simpleapi/client.go
+++ simpleapi/client.go
@@ -1,24 +1,10 @@
 package simpleapi
 
 import (
-	"net/url"
-
-	"github.com/herb-go/fetch"
+	"github.com/herb-go/herb/service/httpservice/fetcher"
 )
 
 type Client struct {
-	EndPoint *fetch.EndPoint
-	Clients  *fetch.Clients
-}
-
-func (c *Client) FetchRequest(params url.Values, body []byte) (resp *fetch.Result, err error) {
-	return c.Clients.FetchWithError(c.EndPoint.NewRequest(params, body))
-}
-
-func (c *Client) FetchJSONRequest(params url.Values, v interface{}) (resp *fetch.Result, err error) {
-	return c.Clients.FetchWithError(c.EndPoint.NewJSONRequest(params, v))
-}
-
-func (c *Client) FetchXMLRequest(params url.Values, v interface{}) (resp *fetch.Result, err error) {
-	return c.Clients.FetchWithError(c.EndPoint.NewXMLRequest(params, v))
+	EndPoint fetcher.TargetGetter
+	Clients  fetcher.Client
 }
